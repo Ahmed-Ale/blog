c9fdbc0b67b45aa192d36c38d83d6e1241c64254
Ahmed-Ale <ahmed19aly@gmail.com>
2024-02-02 01:09:12 +0200

    added index view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ThemeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::controller(ThemeController::class)->name('theme')->group(function () {
+    Route::get('/index', 'index')->name('index');
+});
+
+
+
+
 
 Route::get('/', function () {
     return view('welcome');
